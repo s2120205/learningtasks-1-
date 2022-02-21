2b3cb64df1f61770bad18aac895945989ee8caca
s2120205 <100134931+s2120205@users.noreply.github.com>
2022-02-21 22:43:48 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <h1>Welcome to My Website! Iam Jim!</h1>
     <ul>
         <li><a href="index.php">HOME</a></li>
-        <li><a href="/LT2/index.html">LEARNING TASK 2</a></li
+        <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
     </ul>
     </body>
 </html>
