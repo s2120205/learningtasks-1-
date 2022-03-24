2f8a14b010f3c28ad032bde8adc405048ba0e5f3
s2120205 <100134931+s2120205@users.noreply.github.com>
2022-03-24 23:54:06 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
       <li><a href="index.php">HOME</a></li>
       <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
       <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
-      <li><a href="/LT3/index.html">CLEMT1</a></li>
+      <li><a href="/LT3/index.html">CLE1MT</a></li>
     </ul>
   </body>
 </html>
