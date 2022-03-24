546cecdf8682cb78dbc184db70c048e462112041
s2120205 <100134931+s2120205@users.noreply.github.com>
2022-03-25 01:15:14 +0800

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <li><a href="index.php">HOME</a></li>
         <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
         <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
-        <li><a href="/CLEMT1/index.html">CLMT1</a></li>
+        <li><a href="/CLEMT1/index.html">CLEMT1</a></li>
     </ul>
     </body>
 </html>
