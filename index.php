513df8df7377e440caa27d4900cbd27d8d4a8239
s2120205 <100134931+s2120205@users.noreply.github.com>
2022-03-25 00:27:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,10 +6,10 @@
   <body>
     <h1>Welcome to My Website! I am Jim!</h1>
     <ul>
-      <li><a href="index.php">HOME</a></li>
-      <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
-      <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
-      <li><a href="/CLE1MT/index.html">CLE1MT</a></li>
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+        <li><a href="/LT3/index.html">LEARNING TASK 3</a></li>
+        <li><a href="/CLE1MT/index.html">CLE1MT</a></li>
     </ul>
   </body>
 </html>
