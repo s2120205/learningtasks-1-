751b7737c4701286b383bb19967d9cb77c3ef9ec
s2120205 <100134931+s2120205@users.noreply.github.com>
2022-04-25 09:26:40 +0800

    Add files via upload

---
+++ LT5/index.js
@@ -0,0 +1,24 @@
+function choice1() {
+  document.getElementById("Choice1").src = "button.png";
+}
+function choice1(){
+  document.getElementById("Choice1").src = "button.png";
+}
+function cndclick1(){
+  document.getElementById("Choice1").src = "oil.jpg";
+  var audio = new Audio('pokemon.mp3');
+  audio.play();
+}
+
+ 
+function choice2() {
+  document.getElementById("Choice2").src = "button.png";
+}
+function choice2(){
+  document.getElementById("Choice2").src = "button.png";
+}
+function cndclick2(){
+  document.getElementById("Choice2").src = "butter.jpg";
+  var audio = new Audio('pokemon.mp3');
+  audio.play();
+}
